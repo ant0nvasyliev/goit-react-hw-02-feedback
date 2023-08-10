7c62b7b42ef30abd8e694e7a57cd62cc0c22b953
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-08-10 09:47:05 +0100

    1

---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,11 @@
+import { FeedbackWrapper } from "./Feedback.styled"
+
+export const Feedback = () => {
+   return (
+      <>
+      <FeedbackWrapper>
+         <p>text</p>
+      </FeedbackWrapper>
+      </>
+   )
+}
---
+++ src/components/feedback/Feedback.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FeedbackWrapper = styled.div`
+width: 400px;
+height: 400px;
+background-color: tomato;
+`;
